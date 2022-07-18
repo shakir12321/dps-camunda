556f9d89863b170721f8795463e1ef5994603f75
Shakir <sshakir56@gmail.com>
2022-07-18 15:20:02 +0530

    Working POM file with Depedencies resolved

---
+++ src/test/java/com/dps/dps/DpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dps.dps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
